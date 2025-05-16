b52a5b65592ee20eeb0d6b2b3978f392601aef8c
memoowi <megatenlike@gmail.com>
2025-05-16 22:21:13 +0700

    fix: salary component dual pagination bug

--- app/Livewire/SalaryComponent.php
+++ app/Livewire/SalaryComponent.php
@@ -23,9 +23,9 @@ class SalaryComponent extends Component
     public function render()
     {
         $allowances = Allowance::latest()->paginate(5);
-        return view('livewire.admin.salary-component',[
-            'allowances' => Allowance::latest()->paginate(5),
-            'deductions' => Deduction::latest()->paginate(5),
+        return view('livewire.admin.salary-component', [
+            'allowances' => Allowance::latest()->paginate(5, ['id', 'name', 'amount', 'rule'], 'allowances'),
+            'deductions' => Deduction::latest()->paginate(5, ['id', 'name', 'amount'], 'deductions'),
         ]);
     }
     public function openModal($modalType, $selectedId = null)
@@ -59,31 +59,32 @@ class SalaryComponent extends Component
     public function addAllowance()
     {
         $valdiated = $this->validate([
-           'name' => ['required','min:3','max:255','string','unique:allowances,name'], 
-           'description' => ['required','string','max:1000'],
-           'amount' => ['required','numeric'],
-           'rule' => ['required','in:fixed,percentage'],
+            'name' => ['required', 'min:3', 'max:255', 'string', 'unique:allowances,name'],
+            'description' => ['required', 'string', 'max:1000'],
+            'amount' => ['required', 'numeric'],
+            'rule' => ['required', 'in:fixed,percentage'],
         ]);
 
         $allowance = Allowance::create($valdiated);
         Toaster::success('Allowance added successfully');
         $this->closeModal();
         $this->modal('main-modal')->close();
+        $this->resetPage('allowances');
     }
     public function updateAllowance()
     {
         $valdiated = $this->validate([
-            'name' => ['required','min:3','max:255','string','unique:allowances,name,'.$this->selectedId], 
-            'description' => ['required','string','max:1000'],
-            'amount' => ['required','numeric'],
-            'rule' => ['required','in:fixed,percentage'],
-         ]);
- 
-         $allowance = Allowance::find($this->selectedId);
-         $allowance->update($valdiated);
-         Toaster::success('Allowance updated successfully');
-         $this->closeModal();
-         $this->modal('main-modal')->close();
+            'name' => ['required', 'min:3', 'max:255', 'string', 'unique:allowances,name,' . $this->selectedId],
+            'description' => ['required', 'string', 'max:1000'],
+            'amount' => ['required', 'numeric'],
+            'rule' => ['required', 'in:fixed,percentage'],
+        ]);
+
+        $allowance = Allowance::find($this->selectedId);
+        $allowance->update($valdiated);
+        Toaster::success('Allowance updated successfully');
+        $this->closeModal();
+        $this->modal('main-modal')->close();
     }
     public function openDeleteModal($modalType, $selectedId, $selectedName)
     {
@@ -98,33 +99,35 @@ class SalaryComponent extends Component
         Toaster::success('Allowance deleted successfully');
         $this->closeModal();
         $this->modal('delete-modal')->close();
+        $this->resetPage('allowances');
     }
     public function addDeduction()
     {
         $valdiated = $this->validate([
-            'name' => ['required','min:3','max:255','string','unique:deductions,name'], 
-            'description' => ['required','string','max:1000'],
-            'amount' => ['required','numeric'],
-         ]);
- 
-         $deduction = Deduction::create($valdiated);
-         Toaster::success('Deduction added successfully');
-         $this->closeModal();
-         $this->modal('main-modal')->close();
+            'name' => ['required', 'min:3', 'max:255', 'string', 'unique:deductions,name'],
+            'description' => ['required', 'string', 'max:1000'],
+            'amount' => ['required', 'numeric'],
+        ]);
+
+        $deduction = Deduction::create($valdiated);
+        Toaster::success('Deduction added successfully');
+        $this->closeModal();
+        $this->modal('main-modal')->close();
+        $this->resetPage('deductions');
     }
     public function updateDeduction()
     {
         $valdiated = $this->validate([
-            'name' => ['required','min:3','max:255','string','unique:deductions,name,'.$this->selectedId], 
-            'description' => ['required','string','max:1000'],
-            'amount' => ['required','numeric'],
-         ]);
- 
-         $deduction = Deduction::find($this->selectedId);
-         $deduction->update($valdiated);
-         Toaster::success('Deduction updated successfully');
-         $this->closeModal();
-         $this->modal('main-modal')->close();
+            'name' => ['required', 'min:3', 'max:255', 'string', 'unique:deductions,name,' . $this->selectedId],
+            'description' => ['required', 'string', 'max:1000'],
+            'amount' => ['required', 'numeric'],
+        ]);
+
+        $deduction = Deduction::find($this->selectedId);
+        $deduction->update($valdiated);
+        Toaster::success('Deduction updated successfully');
+        $this->closeModal();
+        $this->modal('main-modal')->close();
     }
     public function deleteDeduction()
     {
@@ -132,5 +135,6 @@ class SalaryComponent extends Component
         Toaster::success('Deduction deleted successfully');
         $this->closeModal();
         $this->modal('delete-modal')->close();
+        $this->resetPage('deductions');
     }
 }
