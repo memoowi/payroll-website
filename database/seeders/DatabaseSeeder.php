047a461f5e27f40729c0a3da1ad89e46b16695c9
memoowi <megatenlike@gmail.com>
2025-05-16 23:30:02 +0700

    feat: Tax PPh seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -35,6 +35,7 @@ class DatabaseSeeder extends Seeder
         //     DepartmentPositionSeeder::class,
         //     AllowanceSeeder::class,
         //     DeductionSeeder::class,
+        //     TaxSeeder::class,
         // ]);
     }
 }
