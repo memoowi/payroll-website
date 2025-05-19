e6e9cd5ab5f59c1055403765423df421264be275
memoowi <megatenlike@gmail.com>
2025-05-19 21:44:16 +0700

    feat: add request leave on employee dashboard

--- app/Livewire/RequestLeave.php
+++ app/Livewire/RequestLeave.php
@@ -2,12 +2,73 @@
 
 namespace App\Livewire;
 
+use App\Models\LeaveRequest;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Livewire\WithPagination;
+use Masmerise\Toaster\Toaster;
 
 class RequestLeave extends Component
 {
+    use WithPagination;
+    public $isEditting = false;
+    public $leaveRequestId = '';
+    public $leaveType = '';
+    public $startDate = '';
+    public $endDate = '';
+    public $reason = '';
     public function render()
     {
-        return view('livewire.employee.request-leave');
+        return view('livewire.employee.request-leave', [
+            'leaveRequests' => Auth::user()->employee->leaveRequests()->latest()->paginate(10),
+        ]);
+    }
+    public function openModal($id = null)
+    {
+        if ($id) {
+            $this->isEditting = true;
+            $this->leaveRequestId = $id;
+            $leaveRequest = LeaveRequest::find($id);
+        }
+        $this->modal('main-modal')->show();
+    }
+    public function closeModal()
+    {
+        $this->reset();
+    }
+    public function save()
+    {
+        $this->validate([
+            'leaveType' => 'required|string|in:sick,vacation,personal,other',
+            'startDate' => 'required|date',
+            'endDate' => 'required|date|after_or_equal:startDate',
+            'reason' => 'nullable|string|max:255',
+        ]);
+
+        if ($this->isEditting) {
+            $leaveRequest = LeaveRequest::find($this->leaveRequestId);
+            if($leaveRequest->status !== 'pending') {
+                Toaster::error('You can only edit leave requests on pending.');
+                return;
+            }
+            $leaveRequest->update([
+                'leave_type' => $this->leaveType,
+                'start_date' => $this->startDate,
+                'end_date' => $this->endDate,
+                'reason' => $this->reason,
+            ]);
+        } else {
+            LeaveRequest::create([
+                'employee_id' => Auth::user()->employee->id,
+                'leave_type' => $this->leaveType,
+                'start_date' => $this->startDate,
+                'end_date' => $this->endDate,
+                'reason' => $this->reason,
+            ]);
+        }
+
+        Toaster::success('Leave request saved successfully!');
+        $this->modal('main-modal')->close();
+        $this->closeModal();
     }
 }
--- resources/views/livewire/employee/request-leave.blade.php
+++ resources/views/livewire/employee/request-leave.blade.php
@@ -1,3 +1,73 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <x-page-heading headingText="Request Leave" descText="Manage your leave requests and approvals" />
+
+    <flux:button icon="plus" variant="primary" type="button" class="w-fit" wire:click="openModal">
+        {{ __('Request Leave') }}
+    </flux:button>
+
+
+
+
+    {{-- Main Modal --}}
+    <flux:modal wire:close="closeModal" name="main-modal" class="md:w-96">
+        <form wire:submit="save" class="space-y-6">
+            <div>
+                <flux:heading size="lg">
+                    @if ($isEditting)
+                        Edit
+                    @else
+                        New
+                    @endif Leave Request
+                </flux:heading>
+                <flux:text class="mt-2">
+                    @if ($isEditting)
+                        You're about to edit this leave request.
+                    @else
+                        You're about to create a new leave request.
+                    @endif
+                </flux:text>
+            </div>
+            
+            <flux:select label="Leave Type" wire:model="leaveType" placeholder="Choose Leave Type..." required>
+                <flux:select.option value="sick">Sick</flux:select.option>
+                <flux:select.option value="vacation">Vacation</flux:select.option>
+                <flux:select.option value="personal">Personal</flux:select.option>
+                <flux:select.option value="other">Other</flux:select.option>
+            </flux:select>
+            <flux:input wire:model="startDate" label="Start Date" type="date" required />
+            <flux:input wire:model="endDate" label="End Date" type="date" required />
+            <flux:textarea wire:model="reason" label="Reason" placeholder="Reason" required />
+
+            <div class="flex">
+                <flux:spacer />
+                <flux:button type="submit" variant="primary">Save</flux:button>
+            </div>
+        </form>
+    </flux:modal>
+
+    {{-- Modal Delete --}}
+    <flux:modal name="delete-modal" class="min-w-[22rem]" wire:close="closeModal">
+        <form 
+            wire:submit="deleteOvertime"
+        class="space-y-6">
+            <div>
+                <flux:heading size="lg">Delete
+                    {{-- {{ $name }} --}}
+                    ?
+                </flux:heading>
+                <flux:text class="mt-2">
+                    <p>You're about to delete this overtime.</p>
+                    <p>This action cannot be reversed.</p>
+                </flux:text>
+            </div>
+            <div class="flex gap-2">
+                <flux:spacer />
+                <flux:modal.close>
+                    <flux:button variant="ghost">Cancel</flux:button>
+                </flux:modal.close>
+                <flux:button type="submit" variant="danger">
+                    Delete</flux:button>
+            </div>
+        </form>
+    </flux:modal>
 </div>
